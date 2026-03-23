75e45a2158d8d528fc4c76e47a70c84777e12834
Fersita09 <fernandabrenesaguilar@gmail.com>
2026-03-23 12:48:55 -0600

    6 sentencias lógicas de pycharm

---
+++ script.py
@@ -0,0 +1,44 @@
+edad= 18
+tieneDPI = 1
+
+if edad >=18:
+    print("Mayor de edad")
+
+nombre = "Seah"
+apellido = "Imeri"
+
+if nombre == "Seah" and apellido == "Imeri":
+   print ("Bienvenido Seah Imeri")
+
+Menu1= "Ceviche"
+Menu2= "Camarones al ajillo"
+
+if Menu1 == "Ceviche" and Menu2 == "Camarones al ajillo":
+    print ("El Menú de hoy es Ceviche y Camarones al ajillo")
+
+deporte1= "Fútbol"
+deporte2= "VolleyBall"
+if deporte1=="Fútbol" and deporte2=="VolleyBall":
+    print ("Los deportes a jugar son Fútbol y VolleyBall")
+
+P1= "Andrea comiendo"
+P2= "Nahomy hablando"
+if P1=="Andrea comiendo" and P2=="Nahomy hablando":
+    print("Andrea está comiendo y Nahomy hablando")
+
+
+Fruta1= "Fresas"
+Fruta2= "Mangos"
+if Fruta1=="Fresas" and Fruta2=="Mangos":
+    print ("Hay Mangos y Fresas en la casa")
+
+Nombre = "Carlos"
+Apellido = "García"
+if nombre=="Carlos" and apellido=="Garcia":
+    print ("Bienvenido Carlos Garcia")
+
+nombre1= "Santiago"
+nombre2= "José"
+if nombre1=="Santiago" and nombre2=="José":
+    print ("Santiago y José están hablando")
+
